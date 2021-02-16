ff4407513d791595f8e76bc95b31a4531bde8f51
Ruben Sørensen <ruben.sorensen98@gmail.com>
2021-02-16 23:12:47 +0100

    Initial commit

--- rubiks/src/Cube.cpp
+++ rubiks/src/Cube.cpp
@@ -11,7 +11,7 @@ void Cube::showSolved() {
 	std::cout << sp << "B B B" << sp << "R R R" << sp << "G G G" << sp << "O O O" << std::endl;
 	std::cout << sp << "B B B" << sp << "R R R" << sp << "G G G" << sp << "O O O" << std::endl;
 	std::cout << sp << "B B B" << sp << "R R R" << sp << "G G G" << sp << "O O O" << std::endl;
-
+	
 	std::cout << sp << "     " << sp << "W W W" << sp << "     " << sp << "     " << std::endl;
 	std::cout << sp << "     " << sp << "W W W" << sp << "     " << sp << "     " << std::endl;
 	std::cout << sp << "     " << sp << "W W W" << sp << "     " << sp << "     " << std::endl;
