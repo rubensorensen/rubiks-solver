bbdce090cd419b0a6defaa01d415cf4f63608425
Ruben Sørensen <ruben.sorensen98@gmail.com>
2021-12-28 18:23:50 +0100

    Simplified shuffle function

--- src/cube_handler.cpp
+++ src/cube_handler.cpp
@@ -7,7 +7,7 @@ void CubeHandler::ShuffleCube(uint8_t n)
     std::vector<int32_t> moves(n);
     std::random_device dev;
     std::mt19937 rng(dev());
-    std::uniform_int_distribution<std::mt19937::result_type> dist(0, 18);
+    std::uniform_int_distribution<std::mt19937::result_type> dist(0, 17);
     std::for_each(moves.begin(), moves.end(),
                   [&dist, &rng](int32_t& i) { i = dist(rng); });
 
@@ -17,65 +17,7 @@ void CubeHandler::ShuffleCube(uint8_t n)
     std::cout << std::endl;
 
     for (auto& m : moves)
-    {
-        switch (m)
-        {
-        case 0:
-            m_Cube->Twist(Cube::MOVE::UP);
-            break;
-        case 1:
-            m_Cube->Twist(Cube::MOVE::UPRIME);
-            break;
-        case 2:
-            m_Cube->Twist(Cube::MOVE::UP2);
-            break;
-        case 3:
-            m_Cube->Twist(Cube::MOVE::DOWN);
-            break;
-        case 4:
-            m_Cube->Twist(Cube::MOVE::DPRIME);
-            break;
-        case 5:
-            m_Cube->Twist(Cube::MOVE::DOWN2);
-            break;
-        case 6:
-            m_Cube->Twist(Cube::MOVE::FRONT);
-            break;
-        case 7:
-            m_Cube->Twist(Cube::MOVE::FPRIME);
-            break;
-        case 8:
-            m_Cube->Twist(Cube::MOVE::FRONT2);
-            break;
-        case 9:
-            m_Cube->Twist(Cube::MOVE::BACK);
-            break;
-        case 10:
-            m_Cube->Twist(Cube::MOVE::BPRIME);
-            break;
-        case 11:
-            m_Cube->Twist(Cube::MOVE::BACK2);
-            break;
-        case 12:
-            m_Cube->Twist(Cube::MOVE::RIGHT);
-            break;
-        case 13:
-            m_Cube->Twist(Cube::MOVE::RPRIME);
-            break;
-        case 14:
-            m_Cube->Twist(Cube::MOVE::RIGHT2);
-            break;
-        case 15:
-            m_Cube->Twist(Cube::MOVE::LEFT);
-            break;
-        case 16:
-            m_Cube->Twist(Cube::MOVE::LPRIME);
-            break;
-        case 17:
-            m_Cube->Twist(Cube::MOVE::LEFT2);
-            break;
-        }
-    }
+        m_Cube->Twist(static_cast<Cube::MOVE>(m));
 }
 
 void CubeHandler::RotateByString(std::string& str)
@@ -83,21 +25,7 @@ void CubeHandler::RotateByString(std::string& str)
     std::stringstream ss(str);
     while (getline(ss, str, ','))
     {
-        if (str == "F")
-            m_Cube->Twist(Cube::MOVE::FRONT);
-        else if (str == "F'")
-            m_Cube->Twist(Cube::MOVE::FPRIME);
-        else if (str == "F2")
-            m_Cube->Twist(Cube::MOVE::FRONT2);
-
-        else if (str == "B")
-            m_Cube->Twist(Cube::MOVE::BACK);
-        else if (str == "B'")
-            m_Cube->Twist(Cube::MOVE::BPRIME);
-        else if (str == "B2")
-            m_Cube->Twist(Cube::MOVE::BACK2);
-
-        else if (str == "U")
+        if (str == "U")
             m_Cube->Twist(Cube::MOVE::UP);
         else if (str == "U'")
             m_Cube->Twist(Cube::MOVE::UPRIME);
@@ -110,6 +38,20 @@ void CubeHandler::RotateByString(std::string& str)
             m_Cube->Twist(Cube::MOVE::DPRIME);
         else if (str == "D2")
             m_Cube->Twist(Cube::MOVE::DOWN2);
+
+        else if (str == "F")
+            m_Cube->Twist(Cube::MOVE::FRONT);
+        else if (str == "F'")
+            m_Cube->Twist(Cube::MOVE::FPRIME);
+        else if (str == "F2")
+            m_Cube->Twist(Cube::MOVE::FRONT2);
+
+        else if (str == "B")
+            m_Cube->Twist(Cube::MOVE::BACK);
+        else if (str == "B'")
+            m_Cube->Twist(Cube::MOVE::BPRIME);
+        else if (str == "B2")
+            m_Cube->Twist(Cube::MOVE::BACK2);
 
         else if (str == "R")
             m_Cube->Twist(Cube::MOVE::RIGHT);
